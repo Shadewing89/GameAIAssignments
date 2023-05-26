8abdc97844a33ea442c92e2e1e7c53e287523dfe
Ilkka Reponen <79361272+Shadewing89@users.noreply.github.com>
2023-05-26 15:46:10 +0300

    Added folders and start on 1

---
+++ Assets/Scripts/Assignment 1/AgentNavToDestination.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+//This script will set the destination for the agent's navmesh navigation
+public class AgentNavToDestination : MonoBehaviour
+{
+    public Transform navigationTarget;
+    private NavMeshAgent agent;
+    
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    
+    void Update()
+    {
+        agent.SetDestination(navigationTarget.position);
+    }
+}
