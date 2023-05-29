ee8e0ea6eecb2959e2d286f866de78315a9f5793
Ilkka Reponen <79361272+Shadewing89@users.noreply.github.com>
2023-05-29 12:57:41 +0300

    edits to animation times

--- Assets/Scripts/Assignment 1/AgentAnimationControl.cs
+++ Assets/Scripts/Assignment 1/AgentAnimationControl.cs
@@ -3,55 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-//Script tries to link navmesh movement to animation using example in unity documentation:
-//https://docs.unity3d.com/Manual/nav-CouplingAnimationAndNavigation.html
-[RequireComponent(typeof(NavMeshAgent))]
-//[RequireComponent(typeof(Animator))]
+//Script tries to link navmesh movement and situations to correct animation
+
 public class AgentAnimationControl : MonoBehaviour
 {
     public Animator anim;
-    NavMeshAgent agent;
-    Vector2 smoothDeltaPosition = Vector2.zero;
-    Vector2 velocity = Vector2.zero;
-
+    Vector3 previousPos;
+    private bool moving;
     void Start()
     {
-        //anim = GetComponent<Animator>();
-        agent = GetComponent<NavMeshAgent>();
-        // Don’t update position automatically
-        agent.updatePosition = false;
+
     }
 
     void Update()
     {
-        Vector3 worldDeltaPosition = agent.nextPosition - transform.position;
-
-        // Map 'worldDeltaPosition' to local space
-        float dx = Vector3.Dot(transform.right, worldDeltaPosition);
-        float dy = Vector3.Dot(transform.forward, worldDeltaPosition);
-        Vector2 deltaPosition = new Vector2(dx, dy);
-
-        // Low-pass filter the deltaMove
-        float smooth = Mathf.Min(1.0f, Time.deltaTime / 0.15f);
-        smoothDeltaPosition = Vector2.Lerp(smoothDeltaPosition, deltaPosition, smooth);
-
-        // Update velocity if time advances
-        if (Time.deltaTime > 1e-5f)
-            velocity = smoothDeltaPosition / Time.deltaTime;
-
-        bool shouldMove = velocity.magnitude > 0.5f && agent.remainingDistance > agent.radius;
-
-        // Update animation parameters
-        anim.SetBool("Walk", shouldMove);
-        //anim.SetFloat("velx", velocity.x);
-        //anim.SetFloat("vely", velocity.y);
-
-        //GetComponent<LookAt>().lookAtTargetPosition = agent.steeringTarget + transform.forward;
+        moving = previousPos != transform.position;
+        anim.SetBool("Walk", moving);
+        previousPos = transform.position;
     }
 
-    void OnAnimatorMove()
-    {
-        // Update position to agent position
-        transform.position = agent.nextPosition;
-    }
+
 }
