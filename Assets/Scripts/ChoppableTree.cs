f844f39f9d200cf60044790ccc1aee9e51b0886f
Ilkka Reponen <79361272+Shadewing89@users.noreply.github.com>
2023-06-07 12:53:05 +0300

    assignment 5 wip

--- Assets/Scripts/ChoppableTree.cs
+++ Assets/Scripts/ChoppableTree.cs
@@ -8,9 +8,17 @@ public class ChoppableTree : MonoBehaviour, IHealth
     private float currentHealth;
     public GameObject treeTop;
     public Transform treeLogSpawnPoint;
+    private bool shouldLogSpawn;
     void Start()
     {
-        
+        if (LogPool.SharedInstance == null)
+        {
+            shouldLogSpawn = false;
+        }
+        else if (LogPool.SharedInstance != null)
+        {
+            shouldLogSpawn = true;
+        }
     }
     void Update()
     {
@@ -31,5 +39,18 @@ public class ChoppableTree : MonoBehaviour, IHealth
     {
         treeTop.SetActive(false);
         gameObject.layer = 0; //we change to default layer to get ignored in navigation targeting
+        if (shouldLogSpawn)
+        {
+            GameObject log = LogPool.SharedInstance.GetPooledObject();
+            if (log != null)
+            {
+                log.GetComponent<Rigidbody>().velocity = Vector3.zero; //This resets the movement of the gameobject
+                log.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+                log.transform.position = treeLogSpawnPoint.position;
+                log.transform.rotation = treeLogSpawnPoint.rotation;
+                log.SetActive(true);
+                //log.GetComponent<Rigidbody>().AddForce(bulletShootSpawnLocation.up * bulletSpeed, ForceMode.Impulse); //impulse to get all the energy at once
+            }
+        }
     }
 }
